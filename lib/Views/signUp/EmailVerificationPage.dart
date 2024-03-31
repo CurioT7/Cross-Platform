ca681ac1b6cb56e7678248f1e0ed82eae888f5cb
amraly <amr.shams2015.as@gmail.com>
2024-04-01 00:01:58 +0200

    TODO: email verificationn with backend

--- lib/Views/signUp/EmailVerificationPage.dart
+++ lib/Views/signUp/EmailVerificationPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:curio/Views/Home_screen.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:curio/services/api_service.dart';
 import 'package:curio/Views/signIn/signin.dart';
@@ -31,53 +32,13 @@ class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
     ).then((UserCredential userCredential) async {
       // After creating the user, send an email verification
       await userCredential.user!.sendEmailVerification();
-      token = await userCredential.user!.getIdToken();
-
-      FirebaseAuth.instance.authStateChanges().listen((User? user) async {
-        print("Email Verification Screen: User: $user");
-        if (user != null) {
-          // Add a delay before reloading the user's data
-          await Future.delayed(Duration(seconds: 5));
-          // Reload the user's information
-          await user.reload();
-          user = FirebaseAuth.instance.currentUser;
-          if (user!.emailVerified) {
-            print('Email is verified');
-            // final response = await ApiService().signInWithToken(token!);
-            // if (response['success'] == true) {
-            //   // save the token into secure storage
-            //   final SharedPreferences prefs = await SharedPreferences.getInstance();
-            //   await prefs.setString('token', response['accessToken']);
-            //   // Navigate to the LoginScreen
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (context) => const SignInPage()),
-              );
-            // }
-            // else{
-            //   if(response['message'].contains('email-already-in-use')) {
-            //     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-            //       content: Text('Email already in use'),
-            //     ));
-            //   }else{
-            //     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-            //       content: Text(response['message']),
-            //     ));
-            //   }
-            // }
-          }
-        }
-      });
-    }).catchError((error) {
-      print("Error: $error");
-      String errorMessage = error.toString();
-      if(errorMessage.contains('email-already-in-use')){
-        errorMessage = 'Email already in use';
-      }
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text(errorMessage),
+    }).catchError((e) {
+      print(e);
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('An error occurred while creating the user'),
       ));
     });
-  }
+    }
 
   @override
   Widget build(BuildContext context) {
@@ -95,10 +56,10 @@ class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
                 try{
                 User? user = FirebaseAuth.instance.currentUser;
                 await user!.reload();
+                String? token = await user.getIdToken();
                 user = FirebaseAuth.instance.currentUser;
                 if (user!.emailVerified) {
-                  print('Email is verified');
-                  print('Token: $token');
+
                   // TODO: Sign in with the token to the server
                   // final response = await ApiService().signInWithToken(token!);
                   // if (response['success'] == true) {
@@ -106,8 +67,8 @@ class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
                   //   final SharedPreferences prefs = await SharedPreferences.getInstance();
                   //   await prefs.setString('token', response['accessToken']);
                     // Navigate to the LoginScreen
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (context) => SignInPage()),
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => HomeScreen()),
                     );
                   // }
                 }

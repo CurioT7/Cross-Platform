6c4641b34a7cbaff3044b543824c2a49dc392fc9
Abdulmonem Elsherif <83321464+AbdulmonemElsherif@users.noreply.github.com>
2024-04-17 23:31:46 +0200

    Refactor customSidebar.dart to add favorite communities functionality and fetch communities from API

--- lib/Views/sidebars/customSidebar.dart
+++ lib/Views/sidebars/customSidebar.dart
@@ -1,83 +1,164 @@
-//import 'dart:html';
-
+import 'package:curio/Views/community/createCommunity.dart';
 import 'package:flutter/material.dart';
 import 'package:curio/Views/sidebars/LeftSideBarAll.dart';
-class CustomSidebar extends StatelessWidget {
+import 'package:curio/services/api_service.dart'; 
+import 'package:curio/Models/community_model.dart';
+import 'package:shared_preferences/shared_preferences.dart'; 
+
+ class CustomSidebar extends StatefulWidget {
+  @override
+  _CustomSidebarState createState() => _CustomSidebarState();
+}
+
+class _CustomSidebarState extends State<CustomSidebar> {
+  List<Community> favoriteCommunities = [];
+
+  Future<List<Community>> fetchCommunities(BuildContext context) async {
+    final sharedPrefs = await SharedPreferences.getInstance();
+    String token = sharedPrefs.getString('token')!;
+    print('Fetching communities from user token: $token');
+    final communities = await ApiService().getCommunities(token, context);
+    return communities ?? [];
+  }
+
+  void toggleFavorite(Community community, bool isFavorite) {
+  setState(() {
+    if (isFavorite) {
+      favoriteCommunities.add(community);
+    } else {
+      favoriteCommunities.remove(community);
+    }
+  });
+}
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
-          ExpansionTile(
-            title: Text('Recently Visited'),
+                    ExpansionTile(
+            title: const Text('Recently Visited'),
             children: <Widget>[
               ListTile(
-                title: Text('Subreddit 1'),
+                title: const Text('Subreddit 1'),
                 trailing: IconButton(
-                  icon: Icon(Icons.clear),
+                  icon: const Icon(Icons.clear),
                   onPressed: () {
                     // Implement your delete functionality here
                   },
                 ),
               ),
               ListTile(
-                title: Text('Subreddit 2'),
+                title: const Text('Subreddit 2'),
                 trailing: IconButton(
-                  icon: Icon(Icons.clear),
+                  icon: const Icon(Icons.clear),
                   onPressed: () {
                     // Implement your delete functionality here
                   },
                 ),
               ),
               ListTile(
-                title: Text('See All'),
+                title: const Text('See All'),
                 onTap: () {
 
 
                 },
               ),
             ],
+                      ),
+              if (favoriteCommunities.isNotEmpty)
+                        ExpansionTile(
+                          title: const Text('Favorites'),
+                          children: favoriteCommunities.map((community) {
+                            return ListTile(
+                              title: Text('r/${community.name}'),
+                              trailing: IconButton(
+                                icon: const Icon(Icons.star),
+                                onPressed: () => toggleFavorite(community, false),
+                              ),
+                            );
+                          }).toList(),
+                        ),
+
+          const Divider(),
+          FutureBuilder<List<Community>>(
+            future: fetchCommunities(context),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                if (snapshot.data!.isEmpty) {
+                  return const ListTile(
+                    title: Text('Join communities to see them here'),
+                  );
+                } else {
+                  return ExpansionTile(
+                    title: const Text('Your Communities'),
+                    children: <Widget>[
+                      ListTile(
+                        leading: IconButton(
+                          icon: const Icon(Icons.add),
+                          onPressed: () => {
+                            Navigator.pop(context),
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => createCommunity()),
+                            )
+                          },
+                        ),
+                        title: const Text('Create a Community'),
+                      ),
+                      ...snapshot.data!.map((community) {
+                        return ListTile(
+                          title: Text('r/${community.name}'),
+                          trailing: FavoriteButton(
+              community: community,
+              isFavorite: favoriteCommunities.contains(community),
+              toggleFavorite: toggleFavorite,
+            ),
+                        );
+                      }).toList(),
+                    ],
+                  );
+                }
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                return const CircularProgressIndicator();
+              }
+            },
           ),
-          Divider(),
-          ExpansionTile(
-            title: Text('Your Communities'),
-            children: <Widget>[
-              ListTile(
-                title: Text('Community 1'),
-                trailing: IconButton(
-                  icon: Icon(Icons.star_border),
-                  onPressed: () {
-                    // Implement your favorite functionality here
-                  },
-                ),
-              ),
-              ListTile(
-                title: Text('Community 2'),
-                trailing: IconButton(
-                  icon: Icon(Icons.star_border),
-                  onPressed: () {
-                    // Implement your favorite functionality here
-                  },
-                ),
-              ),
-            ],
-          ),
-          Divider(),
+          const Divider(),
           ListTile(
-            title: Text('All'),
+            title: const Text('All'),
             onTap: () {
-              // Update the state of the app
-              // ...
-              // Then close the drawer
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => AllPage()),
+                MaterialPageRoute(builder: (context) => const AllPage()),
               );
             },
           ),
         ],
       ),
+    );
+  }
+}
+ class FavoriteButton extends StatelessWidget {
+  final Community community;
+  final bool isFavorite;
+  final Function(Community, bool) toggleFavorite;
+
+  FavoriteButton({
+    required this.community,
+    required this.isFavorite,
+    required this.toggleFavorite,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+      onPressed: () {
+        toggleFavorite(community, !isFavorite);
+      },
     );
   }
 }

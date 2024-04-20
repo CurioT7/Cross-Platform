2eccb12d99d25a10de00054bd1831a351edbaf0e
Mahmoudsalah9 <134644754+Mahmoudsalah9@users.noreply.github.com>
2024-04-20 05:42:19 +0200

    share to profile and share to community is done with api and integration to backend

--- lib/Views/share/shareToProfile.dart
+++ lib/Views/share/shareToProfile.dart
@@ -16,29 +16,47 @@ class ShareToProfilePage extends StatefulWidget {
 }
 
 class _ShareToProfilePageState extends State<ShareToProfilePage> {
-  // Instantiate the Post object using the sample data
-  final Post samplePost = Post.fromJson({
-    "_id": "65fba6e0aab809eceb312466",
-    "title": "this is the post title.",
-    "content": "post conntecnt.",
-    "authorName": "Cecile56",
-    "views": 14611,
-    "createdAt": "2024-03-16T03:23:13.002Z",
-    "upvotes": 58542,
-    "downvotes": 11880,
-    "linkedSubreddit": "65fba6dbaab809eceb3123ee",
-    "comments": [],
-    "shares": 91827,
-    "isNSFW": true,
-    "isSpoiler": false,
-    "isOC": true,
-    "isCrosspost": true,
-    "awards": 96140,
-    "media": "https://nayeli.name",
-    "link": "https://brandon.biz",
-    "isDraft": true,
-    "__v": 0
-  });
+  // Define variables to hold post information
+  String postId = '';
+  String postTitle = '';
+  String postContent = '';
+  String authorName = '';
+  String createdAt = '';
+  String mediaUrl = '';
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPostInfo(widget.oldPostId);
+  }
+
+  Future<void> fetchPostInfo(String postId) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+
+    if (token != null) {
+      try {
+        // Call the getInfo function to get post info
+        Map<String, dynamic> postInfo =
+        await ApiServiceMahmoud().getInfo(token, postId, 'post');
+        // Update state with post information
+        setState(() {
+          postId = postInfo['item']['_id'] ?? '';
+          postTitle = postInfo['item']['title'] ?? '';
+          postContent = postInfo['item']['content'] ?? '';
+          authorName = postInfo['item']['authorName'] ?? '';
+          createdAt = postInfo['item']['createdAt'] ?? '';
+          mediaUrl = postInfo['item']['media'] ?? '';
+        });
+      } catch (e) {
+        // Handle errors
+        print('Error fetching post info: $e');
+      }
+    } else {
+      // Handle case when token is not available
+      print('Token not found. Please login again.');
+    }
+  }
 
   Future<void> sharePost(String title, String postId) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -49,7 +67,8 @@ class _ShareToProfilePageState extends State<ShareToProfilePage> {
       print('Token: $token');
       print('Title: $title');
       print('Post ID: $postId');
-      Map<String, dynamic> response = await ApiServiceMahmoud().sharePostToProfile(token, title, postId);
+      Map<String, dynamic> response =
+      await ApiServiceMahmoud().sharePostToProfile(token, title, postId);
       print(response);
       // Show snackbar based on the response
       if (response['success']) {
@@ -70,43 +89,8 @@ class _ShareToProfilePageState extends State<ShareToProfilePage> {
     }
   }
 
-
-  // Function to fetch post info
-  Future<void> fetchPostInfo(String postId) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? token = prefs.getString('token');
-
-    if (token != null) {
-      try {
-        // Call the getInfo function to get post info
-        Map<String, dynamic> postInfo = await ApiServiceMahmoud().getInfo(token, postId, 'post');
-        // Print post info
-        print('Post Info: $postInfo');
-      } catch (e) {
-        // Handle errors
-        print('Error fetching post info: $e');
-      }
-    } else {
-      // Handle case when token is not available
-      print('Token not found. Please login again.');
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    // Call fetchPostInfo function to get post details
-    fetchPostInfo(widget.oldPostId);
-
-    // Extract the necessary data for display
-    String nameofcommunity = samplePost.linkedSubreddit ?? 'Unknown Community';
-    String authorName = samplePost.authorName ?? 'Anonymous';
-    String createdAt = samplePost.createdAt.toString();
-    String mediaUrl = samplePost.media ?? '';
-    String NewSubreddit = 'My profile'; // Initial text
-    // Controller for the TextField
-    TextEditingController titleController =
-    TextEditingController(text: samplePost.title);
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Crosspost'),
@@ -114,8 +98,7 @@ class _ShareToProfilePageState extends State<ShareToProfilePage> {
         actions: [
           GestureDetector(
             onTap: () {
-              // Call the function to share post to profile
-               sharePost(titleController.text, widget.oldPostId);  // Pass the title and old post ID
+              sharePost(postTitle, widget.oldPostId);
             },
             child: Container(
               padding: EdgeInsets.all(10.0),
@@ -133,51 +116,63 @@ class _ShareToProfilePageState extends State<ShareToProfilePage> {
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: <Widget>[
-                CircleAvatar(
-                  backgroundColor: Colors.white,
-                  child: Image.asset('lib/assets/images/Curio.png'),
-                  radius: 30,
-                ),
-                SizedBox(width: 10),
-                Text('My profile'),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ChooseCommunityPage(oldPostId: widget.oldPostId)),
-                    );
-                  },
-                  child: Icon(Icons.arrow_drop_down),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: titleController, // Set the controller
-              onChanged: (newValue) {
-                titleController.text = newValue;
-              },
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
+      body: SingleChildScrollView( // Wrap the Column with SingleChildScrollView
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Image.asset('lib/assets/images/Curio.png'),
+                    radius: 30,
+                  ),
+                  SizedBox(width: 10),
+                  Text('My profile'),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ChooseCommunityPage(
+                            oldPostId: widget.oldPostId,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Icon(Icons.arrow_drop_down),
+                  ),
+                ],
               ),
             ),
-          ),
-          Divider(),
-          ListTile(
-            subtitle: Text('r/$nameofcommunity. u/$authorName. $createdAt'),
-            title: Text(samplePost.title ?? 'No Title'),
-          ),
-          Divider(),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: TextEditingController(text: postTitle),
+                onChanged: (newValue) {
+                  postTitle = newValue;
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Divider(),
+            ListTile(
+              subtitle: Text('u/$authorName. $createdAt'),
+              title: Text(postTitle),
+            ),
+            // Show media if available
+            // Show post content
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(postContent),
+            ),
+            Divider(),
+          ],
+        ),
       ),
     );
   }

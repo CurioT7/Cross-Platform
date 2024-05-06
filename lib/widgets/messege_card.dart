58e5d0ebfe2486ac77b4806b6958f21781d9d95c
Abdulmonem Elsherif <83321464+AbdulmonemElsherif@users.noreply.github.com>
2024-05-07 02:47:58 +0300

    feat: Update MessageCard to toggle read status and improve UI

--- lib/widgets/messege_card.dart
+++ lib/widgets/messege_card.dart
@@ -2,14 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:curio/Models/message.dart';
 import 'package:intl/intl.dart';
 
-class MessageCard extends StatelessWidget {
+class MessageCard extends StatefulWidget {
   final Message message;
 
   MessageCard({required this.message});
 
+  @override
+  _MessageCardState createState() => _MessageCardState();
+}
+
+class _MessageCardState extends State<MessageCard> {
+  bool _isRead = false;
+
   String get timeAgo {
     final now = DateTime.now();
-    final difference = now.difference(message.timestamp);
+    final difference = now.difference(widget.message.timestamp);
     if (difference.inDays > 0) {
       return '${difference.inDays} days ago';
     } else if (difference.inHours > 0) {
@@ -29,18 +36,34 @@ class MessageCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              message.subject??'No Subject',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16.0,
-              ),
+            Row(
+              children: [
+                IconButton(
+                  icon: Icon(
+                    Icons.mail,
+                    color: _isRead ? Colors.grey : Colors.blue,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _isRead = !_isRead;
+                    });
+                  },
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  widget.message.subject ?? 'No Subject',
+                  style: TextStyle(
+                    fontWeight: _isRead ? FontWeight.normal : FontWeight.bold,
+                    fontSize: 16.0,
+                  ),
+                ),
+              ],
             ),
             SizedBox(height: 10.0),
             Text(
-              message.message,
+              widget.message.message,
               style: TextStyle(
-                fontWeight: FontWeight.bold,
+                fontWeight: _isRead ? FontWeight.normal : FontWeight.bold,
                 fontSize: 14.0,
               ),
             ),
@@ -48,7 +71,7 @@ class MessageCard extends StatelessWidget {
             Row(
               children: [
                 Text(
-                  'u/${message.sender.username}',
+                  'u/${widget.message.sender.username}',
                   style: TextStyle(
                     color: Colors.grey,
                     fontSize: 12.0,

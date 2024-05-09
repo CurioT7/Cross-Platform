53865e644a5f71610663feca5d67a389736f9e3a
YaraIhab2 <yaraihab2@gmail.com>
2024-05-09 03:45:05 +0300

    abdelkader merged items

--- lib/controller/history_cubit/history_state.dart
+++ lib/controller/history_cubit/history_state.dart
@@ -5,3 +5,11 @@ abstract class HistoryState {}
 class HistoryInitial extends HistoryState {}
 
 class ChoseCurrentSort extends HistoryState {}
+
+class GetLoadingState extends HistoryState {}
+
+class GetSuccessState extends HistoryState {}
+
+class GetErrorState extends HistoryState {}
+
+class GetHistoryState extends HistoryState {}
